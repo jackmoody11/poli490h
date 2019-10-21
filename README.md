5ee36566a687868cc3beab0fc39dce850da5271f
jackmoody11 <jackmoody@unc.edu>
2019-10-21 15:52:26 -0400

    Update notebook

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # POLI 490H Data Analysis
 This repository aims to look at how different marijuana-related offenses are treated in courts in North Carolina.
+
+Distinguish between possession/using, selling/delivering, and trafficking
+
+Add meth
+Pick one drug type for first analysis and then you can replicate that process for other types
+To refine the paper, you can look at the different types of drugs/classifications
--- src/main.py
+++ src/main.py
@@ -1,8 +1,12 @@
+from matplotlib.ticker import FuncFormatter
 import pandas as pd
 import numpy as np
 import datetime
 import matplotlib.pyplot as plt
 import csv
+import os
+import sys
+from dateutil.relativedelta import relativedelta
 
 
 def get_fields(field_file):
@@ -10,57 +14,57 @@ def get_fields(field_file):
         return list(csv.reader(f))[0]  # assume only one line
 
 
-def fix_birthdays(x):
-    try:
-        return datetime.datetime.strptime(x, '%Y%m%d')
-    except (ValueError, TypeError):
-        return pd.NaT
+# Read in files and initial data frames, offense codes, and county codes
+df = pd.read_csv('./data/example_arrests.csv', dtype={'CRRKCY': str})
+fields = pd.read_csv('./data/fields.csv', index_col='field')
+marijuana_offense_codes = pd.read_csv(
+    './data/offense_codes_marijuana.csv')['code'].tolist()
+cocaine_offense_codes = pd.read_csv(
+    './data/offense_codes_cocaine.csv')['code'].tolist()
+heroin_offense_codes = pd.read_csv(
+    './data/offense_codes_heroin.csv')['code'].tolist()
+county_codes = pd.read_csv(
+    './data/county_codes.csv', dtype={'code': str}).set_index('code')['county'].to_dict()
+
+df = df[fields.index.tolist()]  # limit data frame to desired fields
+df.rename(columns=fields['description'].to_dict(), inplace=True)
+df = df.replace({'.': np.nan})
+# Make new column with column names based on column numbers
+df['County'] = df['County_Number'].map(county_codes)
+df['Defendant_Race'] = df['Defendant_Race_Code'].map(
+    {'A': 'Asian', 'B': 'Black', 'H': 'Hispanic', 'I': 'Indian', 'O': 'Other', 'U': 'Unknown', 'W': 'White', 'X': 'Non-person'})  # Create new column to map races
+df['Probation_Length'].replace({'.': 0})
+df['Filing_Date'] = pd.to_datetime(df['Filing_Date'])
+df['Year'] = df['File_Number_Century'] * 100 + \
+    df['File_Number_Year']  # Convert century and year to single column
+df.drop(['File_Number_Century', 'File_Number_Year'],
+        inplace=True, axis=1)  # Drop old columns
+df['Defendant_Date_of_Birth'] = pd.to_datetime(
+    df['Defendant_Date_of_Birth'], errors='coerce')
+df['Age'] = (pd.to_datetime(df['Year'], format='%Y').dt.year -
+             df['Defendant_Date_of_Birth'].dt.year)
+
+# Limit to different types of drug offenses
+marijuana = df[df['Charged_Offense_Code'].isin(marijuana_offense_codes)]
+cocaine = df[df['Charged_Offense_Code'].isin(cocaine_offense_codes)]
+heroin = df[df['Charged_Offense_Code'].isin(heroin_offense_codes)]
+
+marijuana_race_offense_code = marijuana.groupby(
+    ['Defendant_Race', 'Charged_Offense_Code']).count()['County'].unstack('Defendant_Race')
+marijuana_offense_code_counts = marijuana_race_offense_code.T.sum()
+marijuana_race_counts = marijuana_race_offense_code.sum()
+
+marijuana_race_pct = marijuana_race_counts / marijuana_race_counts.sum()
 
 
-if __name__ == "__main__":
-    # Define variables
-    df = pd.read_csv('../data/example_arrests.csv', dtype={'CRRKCY': str})
-    fields = pd.read_csv('../data/fields.csv', index_col='field')
-    marijuana_offense_codes = pd.read_csv(
-        '../data/offense_codes_marijuana.csv')['code'].tolist()
-    cocaine_offense_codes = pd.read_csv(
-        '../data/offense_codes_cocaine.csv')['code'].tolist()
-    heroin_offense_codes = pd.read_csv(
-        '../data/offense_codes_heroin.csv')['code'].tolist()
-    county_codes = pd.read_csv(
-        '../data/county_codes.csv', dtype={'code': str}).set_index('code')['county'].to_dict()
+marijuana['Probation_Frame'].replace(
+    {'Y': 365, 'M': 30, 'D': 1, '.': np.nan}, inplace=True)
+marijuana['Probation_Length'].replace({'.': np.nan}, inplace=True)
 
-    df = df[fields.index.tolist()]  # limit data frame to desired fields
-    df.rename(columns=fields['description'].to_dict(), inplace=True)
-    df['County'] = df['County_Number'].map(county_codes)
+marijuana['Probation_Length'] = marijuana['Probation_Length'].replace({
+                                                                      '.': np.nan})
+marijuana['Probation_Length'] = marijuana['Probation_Length'].astype(float)
 
-    # Limit to different types of drug offenses
-    marijuana = df[df['CROFFC'].isin(marijuana_offense_codes)]
-    cocaine = df[df['CROFFC'].isin(cocaine_offense_codes)]
-    heroin = df[df['CROFFC'].isin(heroin_offense_codes)]
-
-    # Replace . with NaN
-    # df.replace({'.': np.nan}, inplace=True)
-
-    # Fix DOB format
-    # df['CRRDOB'] = df['CRRDOB'].apply(lambda x: fix_birthdays(x))
-
-    # # Histogram of race
-    # df['CRRACE'].hist()
-
-    # # Histogram of sex
-    # df['CRRSEX'].hist()
-
-    # # Histogram of home state
-    # df['CRRDST'].hist()
-
-    # # Histogram of defendant age
-    # df['CRRAGE'].hist()
-
-    # # Histogram of DOB
-    # df['CRRDOB'].hist()
-
-    # # Histogram of offense codes
-    # df['CROFFC'].hist(bins=100)
-
-    # plt.show()
+marijuana['Probation_Length'] = marijuana['Probation_Length'] * \
+    marijuana['Probation_Frame']
+marijuana_probation_length = marijuana['Probation_Length'].dropna()
