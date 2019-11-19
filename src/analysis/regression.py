bd497010b069d6e79c2abc4b11ba31da55527b5c
jackmoody11 <jackmoody@unc.edu>
2019-11-18 22:01:21 -0500

    Simplify regression code

--- src/analysis/regression.py
+++ src/analysis/regression.py
@@ -2,6 +2,7 @@ from analysis.base import _Base
 import statsmodels.api as sm
 import pandas as pd
 import matplotlib.pyplot as plt
+from analysis.utils import linear_regression
 
 
 class RegressionAnalysis(_Base):
@@ -10,30 +11,19 @@ class RegressionAnalysis(_Base):
         self.__methods__ = [self.run_regression]
 
     def run_regression(self):
-        for sentence_type, harshness_measure in (('Active', 'Minimum_Sentence_Length_in_Days'), ('Intermediate', 'Probation_in_Days'), ('Community', 'Community_Service_Hours')):
-            if sentence_type == 'Community':
-                _df = self.df[(self.df['Active_Sentence_Indicator'] == sentence_type) & (self.df['Minimum_Sentence_Length_in_Days'].isna())][[
-                    harshness_measure, 'Age', 'Defendant_Race', 'County', 'Plea_Code', 'District_Court_Attorney_Type', 'Defendant_Sex_Code']].dropna(axis=0)
-            else:
-                _df = self.df[(self.df['Active_Sentence_Indicator'] == sentence_type)][[
-                    harshness_measure, 'Age', 'Defendant_Race', 'County', 'Plea_Code', 'District_Court_Attorney_Type', 'Defendant_Sex_Code']].dropna(axis=0)
-            y = _df[harshness_measure]
-            X = pd.concat([_df['Age'], *[pd.get_dummies(_df[var]) for var in ('Defendant_Race',
-                                                                              'County', 'Plea_Code', 'District_Court_Attorney_Type', 'Defendant_Sex_Code')]], axis=1)
-            X = sm.add_constant(X)
-            model = sm.OLS(y, X)
-            results = model.fit()
-            summary = results.summary2()
+        harshness_measure = {'Active': 'Minimum_Sentence_Length_in_Days',
+                             'Intermediate': 'Probation_in_Days',
+                             'Community': 'Community_Service_Hours'}
+        if self.st == 'Community':
+            _df = self.df[(self.df['Active_Sentence_Indicator'] == self.st) & (
+                self.df['Minimum_Sentence_Length_in_Days'].isna())]
+        else:
+            _df = self.df[(self.df['Active_Sentence_Indicator']
+                           == self.st)]
 
-            # plt.rc('figure', figsize=(12, 7))
-            plt.text(0.01, 0.05, str(summary), {
-                     'fontsize': 10}, fontproperties='monospace')
-            plt.axis('off')
-            self.save_figure('regression')
-            print(summary)
-            # results_html = summary.tables[1].as_html()
-            # coef_header = self.drug_name + '_' + sentence_type + '_coef'
-            # p_value_header = self.drug_name + '_' + sentence_type + '_p_value'
-            # series = pd.read_html(results_html, header=0, index_col=0)[0][[
-            #     'coef', 'P>|t|']].rename({'coef': coef_header, 'P>|t|': p_value_header}, axis=1)
-            # self.coefficients = pd.concat([coefficients, series], axis=1)
+        results = linear_regression(_df, harshness_measure[self.st], ['Age'], [
+                                    'Defendant_Race', 'County', 'Plea_Code', 'District_Court_Attorney_Type', 'Defendant_Sex_Code'])
+        plt.text(0.01, 0.05, str(results.summary2()), {
+            'fontsize': 10}, fontproperties='monospace')
+        plt.axis('off')
+        self.save_figure('regression')
--- src/analysis/utils.py
+++ src/analysis/utils.py
@@ -3,11 +3,25 @@ import pandas as pd
 
 
 def linear_regression(df, dep_vars, indep_vars, *dummy_vars):
-    _df = pd.concat([df[dep_vars], df[indep_vars], *[pd.get_dummies(df[dummy_var])
-                                                     for dummy_var in dummy_vars]], axis=1).dropna()
+    _df = pd.concat([df[dep_vars], df[indep_vars],
+                     get_dummies(df, *dummy_vars)], axis=1).dropna()
     X = _df.drop(dep_vars, axis=1)
     X = sm.add_constant(X)
     y = _df[dep_vars]
     model = sm.OLS(y, X)
     results = model.fit()
     return results
+
+
+def get_dummies(df, *dummies):
+    """ Drops certain dummy variable to test hypotheses and prevent multicolinearity. """
+    dfs = []
+    dummy_map = {'Defendant_Race': 'White',
+                 'County': 'WAKE',
+                 'Plea_Code': 'Guilty',
+                 'District_Court_Attorney_Type': 'Privately Retained or Self',
+                 'Defendant_Sex_Code': 'Female'}
+    for dummy in dummies:
+        _df = pd.get_dummies(df[dummy])
+        dfs.append(_df.drop(dummy_map[dummy], axis=1))
+    return pd.concat(dfs, axis=1)
