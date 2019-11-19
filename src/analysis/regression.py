c9670fbf5613067ed5d338374f1576f81e58f2e5
jackmoody11 <jackmoody@unc.edu>
2019-11-18 23:05:55 -0500

    Move regression to init so that info is available from beginning

--- src/analysis/regression.py
+++ src/analysis/regression.py
@@ -8,9 +8,6 @@ from analysis.utils import linear_regression
 class RegressionAnalysis(_Base):
     def __init__(self, df, drug_name, sentence_type):
         super().__init__(df, drug_name, sentence_type)
-        self.__methods__ = [self.run_regression]
-
-    def run_regression(self):
         harshness_measure = {'Active': 'Minimum_Sentence_Length_in_Days',
                              'Intermediate': 'Probation_in_Days',
                              'Community': 'Community_Service_Hours'}
@@ -20,10 +17,15 @@ class RegressionAnalysis(_Base):
         else:
             _df = self.df[(self.df['Active_Sentence_Indicator']
                            == self.st)]
+        self._results = linear_regression(_df, harshness_measure[self.st], ['Age'],
+                                          'Defendant_Race', 'County', 'Plea_Code', 'District_Court_Attorney_Type', 'Defendant_Sex_Code')
+        self.__methods__ = [self.print_regression]
 
-        results = linear_regression(_df, harshness_measure[self.st], ['Age'],
-                                    'Defendant_Race', 'County', 'Plea_Code', 'District_Court_Attorney_Type', 'Defendant_Sex_Code')
-        plt.text(0.01, 0.05, str(results.summary2()), {
+    def print_regression(self):
+        plt.text(0.01, 0.05, str(self._results.summary2()), {
             'fontsize': 10}, fontproperties='monospace')
         plt.axis('off')
         self.save_figure('regression')
+
+    def get_results(self):
+        return self._results
